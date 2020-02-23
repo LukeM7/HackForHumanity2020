1d35f64ec418a67d235cb4420a852397876fef56
togoforfood <ryanshiau1@gmail.com>
2020-02-23 01:18:10 -0800

    WE DID IT BOYS maybe

--- trashy/app.py
+++ trashy/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from PIL import Image
+import os
 import requests
 
 app = Flask(__name__)
@@ -12,12 +13,11 @@ def index():
 @app.route('/processImage', methods=['POST'])
 def processImage():
     if request.method == 'POST':
-        print("testing22")
         image = request.files.get("myFile")
         print(image)
-        im = Image.open(image)#im is a pillow object
-        rgb_im = im.convert('RGB')
-        im.save("userinp.jpg")
+        im = Image.open(image, mode='r')#im is a pillow object
+
+        im.save('userImages/userim.png')
 
     return render_template("home.html")
 
