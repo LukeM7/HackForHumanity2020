8138a77ec1281ad958fafd1dd4d74abdb2fa0d07
benshiao <shiaobenjamin@Gmail.com>
2020-02-23 00:33:53 -0800

    Fix this Ryan

--- trashy/app.py
+++ trashy/app.py
@@ -1,25 +1,31 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-#from flask_bootstrap import bootstrap
+from PIL import Image
 import requests
 
-app = Flask(name)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-def printing(str):
-    str = str+"sheep"
-    print(str)
+@app.route('/processImage', methods=['POST'])
+def processImage():
+    if request.method == 'POST':
+        print("testing22")
+        image = request.files.get("myFile")
+        print(image)
+        im = Image.open(image)#im is a pillow object
+        rgb_im = im.convert('RGB')
+        im.save("userinp.jpg")
+
+    return render_template("home.html")
 
 @app.route("/info", methods=['POST'])
 def echo():
     return render_template('home.html', text=printing(request.form['text']))
 
 
-
-
-if name == 'main':
+if __name__ == '__main__':
     app.secret_key= 'secret123'
     app.run(debug=True)
