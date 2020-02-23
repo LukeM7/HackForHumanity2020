33f03c49bb8468a1e754d3368e12373e8845703a
togoforfood <ryanshiau1@gmail.com>
2020-02-22 22:49:21 -0800

    Last commit from me

--- trashy/app.py
+++ trashy/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from flask_bootstrap import Bootstrap
 import requests
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route('/')
 def index():
